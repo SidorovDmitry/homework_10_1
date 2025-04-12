22f251273f8f241c7a8b86f9ad491d8370cb4949
Dmitri_Sidorov <kin19861@rambler.ru>
2025-04-12 19:21:58 +0300

    add func loging in masks.py and utils.py

--- src/masks.py
+++ src/masks.py
@@ -1,22 +1,49 @@
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler(
+    os.path.join(os.path.dirname(__file__), "../logs", "masks.log"), mode="w", encoding="utf-8"
+)
+
+
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(funcName)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+# logger.debug("Debug message")
+# logger.info("Info message")
+# logger.warning("Warning message")
+# logger.error("Error message")
+# logger.critical("Critical message")
+
+
 def get_mask_card_number(number_cards: int | str) -> str:
     """Функция принимает на вход номер карты число и возвращает маску номера"""
+    logger.info(get_mask_card_number.__doc__)
     str_cards_number = str(number_cards)  # Преобразуем число в строку
 
     # Проверяем, что номер карты состоит из 16 цифр
     if len(str_cards_number) != 16 or not str_cards_number.isdigit():
+        logger.error("Некорректный номер карты")
         return "Некорректный номер карты"
 
     # Формируем маску номера карты
     return f"{str_cards_number[:4]} {str_cards_number[4:6]}** **** {str_cards_number[-4:]}"
+    logger.info("OK")
 
 
 def get_mask_account(account_number: int | str) -> str:
     """Функция принимает на вход номер счета в виде числа и возвращает маску номера."""
+    logger.info(get_mask_card_number.__doc__)
     account_number_str = str(account_number)  # Преобразуем номер счёта в строку
 
     # Проверяем, что номер счёта состоит только из цифр и имеет достаточную длину
     if len(account_number_str) != 20 or not account_number_str.isdigit():
+        logger.error("Некорректный номер счёта")
         return "Некорректный номер счёта"
 
     # Формируем маску номера счёта
     return "**" + account_number_str[-4:]
+    logger.info("OK")
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,24 @@
 import json
+import logging
 import os
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler(
+    os.path.join(os.path.dirname(__file__), "../logs", "utils.log"), mode="w", encoding="utf-8"
+)
+
+
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(funcName)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+# logger.debug("Debug message")
+# logger.info("Info message")
+# logger.warning("Warning message")
+# logger.error("Error message")
+# logger.critical("Critical message")
+
 
 # Получаем путь к текущему скрипту
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -10,19 +29,24 @@ file_path = os.path.join(script_dir, "../data/operations.json")
 
 def read_file(filename=None):
     """Функция для чтения JSON-файла и обработки возможных ошибок при его открытии и чтении"""
-
+    logger.info(read_file.__doc__)
     try:
+
         with open(filename, "r", encoding="utf-8") as f:
+            logger.info("Открытие JSON-файла")
             data = json.load(f)
 
         # Проверяем, что данные представляют собой список
         if not isinstance(data, list):
+            logger.error("Ошибка обработки файла")
             return []
 
         return data
-    except FileNotFoundError:
+    except FileNotFoundError as e:
+        logger.error(f"Файл не найден {e}")
         print(f"Файл не найден по пути: {filename}")
         return []
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as e:
+        logger.error(f"Ошибка при декодировании JSON из файла {e}")
         print(f"Ошибка при декодировании JSON из файла: {filename}")
         return []
