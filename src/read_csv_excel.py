9eb72c4998c53e94fc14de8b4f037179f7bfe5b0
Dmitri_Sidorov <kin19861@rambler.ru>
2025-04-15 17:12:39 +0300

    updata read_csv_excel.py

--- src/read_csv_excel.py
+++ src/read_csv_excel.py
@@ -2,10 +2,8 @@ import os
 import csv
 import pandas as pd
 
-from src.generators import transaction_descriptions
 
 file_path_csv = os.path.join(os.path.dirname(__file__), "../data", "transactions.csv")
-
 file_path_excel = os.path.join(os.path.dirname(__file__), "../data", "transactions_excel.xlsx")
 
 
@@ -14,10 +12,10 @@ def read_csv_file(file_path, delimiter):
     result = []
     try:
         with open(file_path, "r", encoding="utf-8") as file:
-            reader = csv.DictReader(file, delimiter=delimiter)  # Читаем как словари
+            reader = csv.DictReader(file, delimiter=delimiter)  # Читаем как словари через библиотеку CSV
             for row in reader:
                 result.append(row)
-        return (result)
+        return result
 
     except FileNotFoundError:
         print(f"Файл не найден по пути: {file_path}")
@@ -30,14 +28,13 @@ def read_csv_file(file_path, delimiter):
 # print(read_csv_file(file_path_csv,";")) # Для проверки работы функции
 
 
-def read_excel_file(file_path) :
+def read_excel_file(file_path):
     """Функция для считывания финансовых операций из XLSX-файла и возврата списка словарей."""
     try:
 
-        df = pd.read_excel(file_path) # Читаем Excel файл
+        df = pd.read_excel(file_path)  # Читаем Excel файл
         transaction_list = df.to_dict(orient="records")  # Преобразуем DataFrame в список словарей
         return transaction_list
-
 
     except FileNotFoundError:
         print(f"Файл не найден по пути: {file_path}")
